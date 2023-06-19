e022fd04966804f338155a5071ab6aee07da3539
Albenit <albenit44@gmail.com>
2023-06-19 17:12:11 +0200

    Design of chat implemented

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,8 +1,39 @@
 <div>
     <div class="chatlist_header">
-        
+        <div class="title">
+            Chat
+        </div>
+        <div class="img_container">
+            <img class="img" src="https://picsum.photos/200/300?grayscale" alt="">
+        </div>
+
     </div>
     <div class="chatlist_body">
-        
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">
+                <img class="img" src="https://picsum.photos/150/300?grayscale" alt="">
+            </div>
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username">Albenit</div>
+                    <span class="date">3d</span>
+                </div>
+
+                <div class="bottom_row">
+
+                    <div class="message_body text-truncate">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, ad dolor necessitatibus impedit
+                        non porro. Suscipit itaque assumenda dolor ratione omnis inventore consequuntur officiis, aut
+                        iste, debitis sint! Recusandae, ratione!
+                    </div>
+
+                    <div class="unread_count">
+                        56
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,4 +1,5 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-    send
+    <div class="chatbox_footer">
+        
+    </div>
 </div>
