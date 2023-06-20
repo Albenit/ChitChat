660e5d60c58b6614c77413f8774b0bdbca95171b
Albenit <albenit44@gmail.com>
2023-06-20 10:45:00 +0200

    Style of chat added

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,5 +1,10 @@
 <div>
-    <div class="chatbox_footer">
-        
-    </div>
+    <form action="">
+        <div class="chatbox_footer">
+            <div class="costum_form_group">
+                <input type="text" class="control">
+                <button type="submit" class="submit">Send</button>
+            </div>
+        </div>
+    </form>
 </div>
