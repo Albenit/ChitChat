f093290cf5c726b7152397dc8a9c5eb3d7c6f271
Albenit <albenit44@gmail.com>
2023-06-21 11:10:03 +0200

    update the chat scrolling and paginate of messages

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -3,7 +3,7 @@
         <form wire:submit.prevent="sendMessage" action="">
             <div class="chatbox_footer">
                 <div class="costum_form_group">
-                    <input wire:model="body" type="text" class="control">
+                    <input wire:model='body' type="text" class="control" placeholder="Write message">
                     <button type="submit" class="submit">Send</button>
                 </div>
             </div>
